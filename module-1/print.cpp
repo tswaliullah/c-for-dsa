684cbcf3ec3f172bdb0ab6ab1c0fedb05677f384
MD Waliullah <waliullah.godev@gmail.com>
2025-11-16 00:03:53 +0600

    print varible in c++

---
+++ module-1/print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int x = 3;
+    double d = 33.77;
+
+    std::cout << x << " " << d << "\n";
+    std::cout << x << " " << d << std::endl;
+
+    std::cout << "My first serial fav number is: " << x;
+
+    return 0;
+}
