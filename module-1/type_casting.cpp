9f8984cf53a87404ead04b821dbabf447562e7ab
MD Waliullah <waliullah.godev@gmail.com>
2025-11-16 06:49:22 +0600

    type casting

---
+++ module-1/type_casting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    char x = 'a';
+    cout << (int)x << endl;
+
+    char y = 65;
+    cout << (char)y << endl;
+
+    return 0;
+}
