2b75bfdb48ff1ec8aa96cad9523d28671733a2a1
MD Waliullah <waliullah.godev@gmail.com>
2025-11-16 21:42:30 +0600

    ignore enter

--- module-1/ignore_enter.cpp
+++ module-1/ignore_enter.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 int main(){
 
-
+    int x;
     char s[1001];
-    // cin >> s;
 
+    cin >> x;
+    cin.ignore();
     cin.getline(s, 1001);
 
-    cout << s;
+    cout << x << endl << s;
 
 
     return 0;
