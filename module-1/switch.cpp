df5df0f277d7395386bcd62785627a6182134a75
MD Waliullah <waliullah.godev@gmail.com>
2025-11-16 07:22:03 +0600

    switch

---
+++ module-1/switch.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int day;
+    cin >> day;
+    switch (day)
+    {
+    case 1:
+        cout << "Saturday\n";
+        break;
+
+    case 2:
+        cout << "Sunday\n";
+        break;
+
+    case 3:
+        cout << "Monday\n";
+        break;
+    case 4:
+
+        cout << "Monday\n";
+        break;
+    case 5:
+        cout << "Monday\n";
+        break;
+    case 6:
+        cout << "Monday\n";
+        break;
+    case 7:
+        cout << "7Monday\n";
+        break;
+
+    default:
+
+        cout << "Wrong Input\n";
+    }
+
+    return 0;
+}
