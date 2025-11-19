dbac6354b48cee0f1e0ac2e80ec1bc29a7c0bf12
MD Waliullah <waliullah.godev@gmail.com>
2025-11-19 18:23:41 +0600

    Why we need dynamic object

---
+++ module-3/Why we need dynamic object.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int roll, int cls, double gpa)
+    {
+        this->roll = roll;
+        this->cls = cls;
+        (*this).gpa = gpa;
+    }
+};
+
+Student* fun()
+{
+    Student rahim(1, 7, 5);
+
+    Student* k = &rahim;
+
+    return k;
+}
+
+int main()
+{
+
+    Student* obj = fun();
+
+    cout << obj->roll << " " << obj->cls << " " << (*obj).gpa << endl;
+
+    return 0;
+}
