9e3c5deba515be980573bbf0d047c14bb21b4a75
MD Waliullah <waliullah.godev@gmail.com>
2025-11-19 17:33:39 +0600

    Return object from function

---
+++ module-3/Return_object_from_function.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int roll, int cls, double gpa)
+    {
+        this->roll = roll;
+        this->cls = cls;
+        (*this).gpa = gpa;
+    }
+};
+
+Student fun()
+{
+    Student rahim(1, 7, 5);
+
+    return rahim;
+}
+
+int main()
+{
+
+    Student obj = fun();
+
+    cout << obj.roll << " " << obj.cls << " " << obj.gpa << endl;
+
+    return 0;
+}
