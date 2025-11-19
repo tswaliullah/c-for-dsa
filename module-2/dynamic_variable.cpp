a51dc8f01537770f5a8e3b294b224a791fc8e9d2
MD Waliullah <waliullah.godev@gmail.com>
2025-11-19 08:22:05 +0600

    dynamicly handle

--- module-2/dynamic_variable.cpp
+++ module-2/dynamic_variable.cpp
@@ -4,7 +4,21 @@ using namespace std;
 int* p;
 int* q;
 
+void fun()
+{
+    int* y = new int;
 
+    int x = 10;
+    *y = 10;
+
+    p = &x;
+    q = y;
+
+    cout << "Fun X " << *p << endl;
+    cout << "Fun Y " << *q << endl;
+
+    return;
+}
 
 int main()
 {
