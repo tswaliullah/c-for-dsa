9c64545f2bd0d65ab1adaede710eef38b28852a4
MD Waliullah <waliullah.godev@gmail.com>
2025-11-19 08:19:46 +0600

    static-vs-dynamic-memory

---
+++ module-2/dynamic_variable.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int* p;
+int* q;
+
+
+
+int main()
+{
+
+    // int *p = new int;
+
+    // *p = 1010;
+
+    // cout << *p << endl;
+
+    fun();
+
+    cout << "Main X " << *p << endl;
+    cout << "Main Y " << *q << endl;
+    return 0;
+}
