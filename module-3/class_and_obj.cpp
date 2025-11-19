bebe44d02b7c3dd2a93c8c8ae44b129dd1bef49b
MD Waliullah <waliullah.godev@gmail.com>
2025-11-19 16:21:47 +0600

    take input wiht getline

--- module-3/class_and_obj.cpp
+++ module-3/class_and_obj.cpp
@@ -13,15 +13,23 @@ class Student {
 int main()
 {
     
-    Student pias;
-    pias.cgpa = 3.44;
-    pias.roll = 23;
-    char temp [100] = "Shakib";
+    Student pias, juli;
+    // pias.cgpa = 3.44;
+    // pias.roll = 23;
+    // char temp [100] = "Shakib";
+    // strcpy(pias.name, temp);
 
-    strcpy(pias.name, temp);
+    // cin >> pias.name >> pias.roll >> pias.cgpa;
+    // cin >> juli.name >> juli.roll >> juli.cgpa;
 
+    // cout << pias.name << " " << pias.roll << " " << pias.cgpa << endl;
+    // cout << juli.name << " " << juli.roll << " " << juli.cgpa << endl;
+
+    cin.getline(pias.name, 100);
+    cin >> pias.roll >> pias.cgpa;
 
     cout << pias.name << " " << pias.roll << " " << pias.cgpa << endl;
+
     
     return 0;
 }
