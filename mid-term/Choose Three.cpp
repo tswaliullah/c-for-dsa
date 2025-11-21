a2355d412d7ba6f99bf8c74ca9dd02f9980d8ec4
MD Waliullah <waliullah.godev@gmail.com>
2025-11-22 00:06:58 +0600

    choose three

--- mid-term/Choose Three.cpp
+++ mid-term/Choose Three.cpp
@@ -3,8 +3,50 @@ using namespace std;
 
 int main()
 {
-    
-    
-    
+
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+
+        int s;
+        cin >> s;
+
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        int flag = 0;
+
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                for (int l = k + 1; l < n; l++)
+                {
+                    int sum = a[j] + a[k] + a[l];
+                    if (sum == s)
+                    {
+                        flag = 1;
+                    }
+                }
+            }
+        }
+
+        if (flag)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
     return 0;
 }
