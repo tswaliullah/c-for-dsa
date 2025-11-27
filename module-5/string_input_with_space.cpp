bc1e49737cdaa9f36ecaa7e475da049adfdb83ba
MD Waliullah <waliullah.godev@gmail.com>
2025-11-28 00:04:59 +0600

    string_input_with_space

---
+++ module-5/string_input_with_space.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int x;
+    cin >> x;
+
+    string s;
+    cin.ignore();
+    getline(cin, s);
+
+    cout << s << endl;
+
+    return 0;
+}
