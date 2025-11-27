104b4061c5fc3d4154157b767b7230df996e8d39
MD Waliullah <waliullah.godev@gmail.com>
2025-11-27 23:25:04 +0600

    feat: add element_access example demonstrating string manipulation

---
+++ module-5/element_access.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+
+    cin >> s;
+
+    cout << s.at(0) << endl;
+
+
+    cout << s.front() << endl;
+    cout << s.back() << endl;
+    
+    return 0;
+}
