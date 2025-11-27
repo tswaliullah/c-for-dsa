e714d23e7ee81827ad23b115bbb0527be0700f8a
MD Waliullah <waliullah.godev@gmail.com>
2025-11-27 23:48:49 +0600

    feat: add string_modifiers example demonstrating basic string operations

---
+++ module-5/string_modifiers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello World";
+
+    // s[5] = 'A';
+    // s += s2;
+    // s.append(s2);
+    // s.push_back('Hello');
+    // s += 'B';
+    // s.pop_back();
+    // string s3 = "Hi";
+    // s = s3;
+    // s.assign("Gello");
+    // s.erase(3, 5);
+    // s.replace(6, 5, "BD");
+    // s.insert(5, "Pias");
+
+    cout << s << endl;
+    
+    return 0;
+}
