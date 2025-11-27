6cf63219ed6e1578ef7075872a44908167c701f4
MD Waliullah <waliullah.godev@gmail.com>
2025-11-27 23:12:26 +0600

    feat: add string_capacity example with resizing demonstration

---
+++ module-5/string_capacity.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello World as my 7th programming languages";
+
+    // cout << s.size() << endl;
+    // cout << s.max_size()  << endl;
+    // cout << s.capacity() << endl;
+    // s.clear();
+    // s.empty();
+    s.resize(50, 'H');
+
+
+    cout << s << endl;
+
+    return 0;
+}
