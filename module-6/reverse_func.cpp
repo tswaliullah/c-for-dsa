5fdd9bdaa0efe19d4f310e2ae03a4d9f7882b8c3
MD Waliullah <waliullah.godev@gmail.com>
2025-11-29 18:43:02 +0600

    reverse func loop

--- module-6/reverse_func.cpp
+++ module-6/reverse_func.cpp
@@ -4,22 +4,29 @@ using namespace std;
 int main()
 {
 
-    int t;
-    cin >> t;
+    // int t;
+    // cin >> t;
 
-    int a[t];
+    // int a[t];
 
-    for (int i = 0; i < t; i++)
-    {
-        cin >> a[i];
-    }
+    // for (int i = 0; i < t; i++)
+    // {
+    //     cin >> a[i];
+    // }
 
-    reverse(a, a + t);
+    // reverse(a, a + t);
 
-    for (int t : a)
-    {
-        cout << t << endl;
-    }
+    // for (int t : a)
+    // {
+    //     cout << t << endl;
+    // }
+
+    string s;
+    cin >> s;
+
+    reverse(s.begin(), s.end());
+
+    cout << s << endl;
 
     return 0;
 }
