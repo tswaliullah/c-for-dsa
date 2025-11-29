72d26faed5a9014a8dda6af131798a934c49355b
MD Waliullah <waliullah.godev@gmail.com>
2025-11-29 16:57:51 +0600

    range based for looop

---
+++ module-6/range_based_for_loop.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    string s;
+    cin >> s;
+
+    // for (int i = 0; i < s.size(); i++)
+    // {
+    //     cout << s[i] << endl;
+    // }
+
+    for (char t : s)
+    {
+        cout << t << endl;
+    }
+
+    return 0;
+}
