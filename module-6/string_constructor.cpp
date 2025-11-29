c5cf4160edb6fe2e0923331394599b7961f78c02
MD Waliullah <waliullah.godev@gmail.com>
2025-11-29 16:55:19 +0600

    string constructor

---
+++ module-6/string_constructor.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // 1
+    string s("Hello");
+
+    // 2 resize
+    string s2("Hello", 2);
+
+    // 3
+    string s3("Hello");
+    string t(s3, 2);
+
+    // 4
+    string s4(5, 'H');
+
+    cout << s4 << endl;
+
+    return 0;
+}
