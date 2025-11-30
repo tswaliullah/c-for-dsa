18d8f07ac0351487d7c0d8bd73f3e2395e3365b3
MD Waliullah <waliullah.godev@gmail.com>
2025-11-30 14:39:22 +0600

    min arr from obj

--- module-7/min_in_arr_from_an_obj.cpp
+++ module-7/min_in_arr_from_an_obj.cpp
@@ -19,15 +19,20 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        cin.ignore();
-        getline(cin, a[i].name);
-        cin >> a[i].roll >> a[i].marks;
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
     }
+
+    Student mn;
+    mn.marks = INT_MAX;
 
     for (int i = 0; i < n; i++)
     {
-        cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+        if (a[i].marks < mn.marks)
+        {
+            mn = a[i];
+        }
     }
+    cout << mn.name << " " << mn.marks << " " << endl;
 
     return 0;
 }
