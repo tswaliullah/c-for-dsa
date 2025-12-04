12b0673576ce959439d9cb70673991505732ed8d
MD Waliullah <waliullah.godev@gmail.com>
2025-12-04 13:47:08 +0600

    final exam done

--- final/Replace Word.cpp
+++ final/Replace Word.cpp
@@ -3,8 +3,26 @@ using namespace std;
 
 int main()
 {
-    
-    
-    
+
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        string s, x;
+        cin >> s >> x;
+
+        int index = s.find(x);
+        int len = x.length();
+
+        while (index != -1)
+        {
+            s.replace(index, len, "#");
+            index = s.find(x, index + 1);
+        }
+
+        cout << s << endl;
+    }
+
     return 0;
 }
