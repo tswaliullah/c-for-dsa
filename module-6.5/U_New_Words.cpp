b4318eed181470959f57d6db276f1888c798ad65
MD Waliullah <waliullah.godev@gmail.com>
2025-12-04 12:11:08 +0600

    new world

--- module-6.5/U_New_Words.cpp
+++ module-6.5/U_New_Words.cpp
@@ -3,11 +3,39 @@ using namespace std;
 
 int main()
 {
-    
+
     string s;
     cin >> s;
 
+    int e_count = 0, g_count = 0, y_count = 0, p_count = 0, t_count = 0;
 
-    
+    for (int i = 0; i < s.size(); i++)
+    {
+        char ch = s[i];
+        if (ch == 'e' || ch == 'E')
+        {
+            e_count++;
+        }
+        else if (ch == 'g' || ch == 'G')
+        {
+            g_count++;
+        }
+        else if (ch == 'y' || ch == 'Y')
+        {
+            y_count++;
+        }
+        else if (ch == 'p' || ch == 'P')
+        {
+            p_count++;
+        }
+        else if (ch == 't' || ch == 'T')
+        {
+            t_count++;
+        }
+    }
+
+
+    cout << min({e_count, g_count, y_count, p_count, t_count});
+
     return 0;
 }
--- module-6.5/V_Replace_Word.cpp
+++ module-6.5/V_Replace_Word.cpp
@@ -8,7 +8,6 @@ int main()
     cin >> s;
 
     string rs = "EGYPT";
-    
 
     int position = s.find(rs);
 
