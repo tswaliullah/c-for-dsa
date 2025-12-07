2ef775f7e614d43e716bfadc8660de79764e30b4
MD Waliullah <waliullah.godev@gmail.com>
2025-12-07 11:07:32 +0600

    2000

--- module-6.5/B_ICPC_Balloons.cpp
+++ module-6.5/B_ICPC_Balloons.cpp
@@ -4,42 +4,39 @@ using namespace std;
 int main()
 {
 
-    // int tt;
-    // cin >> tt;
+    int t;
+    cin >> t;
 
-    int A = 0, B = 0, C = 0, D = 0, E = 0, F = 0, G = 0, H = 0, I = 0, J = 0,
-        K = 0, L = 0, M = 0, N = 0, O = 0, P = 0, Q = 0, R = 0, S = 0, T = 0,
-        U = 0, vV = 0, W = 0, X = 0, Y = 0, Z = 0;
-
-    string ss;
-    cin >> ss;
-
-    for (int jj = 0; jj < ss.size(); jj++)
+    for (int i = 0; i < t; i++)
     {
+        int n;
+        cin >> n;
 
-        
-        // cout << ss[jj] << endl;
-        if (ss[jj] == 0)
+        string a;
+        cin >> a;
+
+        bool visited[26] = {false};
+
+        int count_ballons = 0;
+
+        for (int j = 0; j < n; j++)
         {
-            ss[jj] += 2;
+            char ch = a[j];
+            int index = ch - 'A';
+
+            if (visited[index] == false)
+            {
+                count_ballons += 2;
+                visited[index] = true;
+            }
+            else
+            {
+                count_ballons++;
+            }
         }
-        else
-        {
-            ss[jj]++;
-        }
+
+        cout << count_ballons << endl;
     }
-    cout << C << endl;
-
-    int sum = 0;
-    for (int jj = 0; jj < ss.size(); jj++)
-    {
-        if (ss[jj] != 0)
-        {
-            sum += sum;
-        }
-    }
-
-    cout << sum;
 
     return 0;
 }
