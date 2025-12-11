8d03964968064493fa5231c51555ab657c1406e2
MD Waliullah <waliullah.godev@gmail.com>
2025-12-11 10:59:49 +0600

    Access Code Equality

---
+++ problem solving week/day-3/Access Code Equality.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    if (s == "WECNITK")
+    {
+        cout << "Welcome to Web Club!";
+    }
+    else
+    {
+        cout << "Access denied";
+    }
+
+    return 0;
+}
